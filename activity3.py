2e008867e954b69f102fd676684f0856af3f25e3
Reda452 <redasakhi0@gmail.com>
2024-12-21 10:12:19 +0100

    new2

---
+++ activity3.py
@@ -0,0 +1,22 @@
+print("chose your ride")
+print("1.bike")
+print("2.car")
+choice=int(input("enter your choice"))
+if choice=="1":
+    print("wich type of bike do you want")
+    print("1 sport bike")
+    print("2 red bike")
+    choice1=int(input("enter your choice"))
+    if choice1=="1":
+        print("you have choice a sport bike")
+    else:
+        print(" you have choice a red bike")
+elif choice=="2":
+    print("wich type of car do you want")
+    print("1 sport car")
+    print("2 red car")
+    choice1=int(input("enter your choice"))
+    if choice1=="1":
+        print("you have choice a sport car")
+    else:
+        print(" you have choice a red car")
